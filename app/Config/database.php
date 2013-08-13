a522cb2b0ddda09df1359da8dbb13d78f5b46357
luizhrqas <luizhrqas@gmail.com>
2013-08-13 17:33:25 -0300

    Database file created.

---
+++ app/Config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => true,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'brazilian-shipping-api',
+		'prefix' => '',
+		'encoding' => 'utf8',
+	);
+
+}
